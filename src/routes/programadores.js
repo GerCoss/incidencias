7d1a7a6cd861f9c1ec948221585921f8e6f74316
GerCoss <gerardocoss92@gmail.com>
2018-10-12 12:15:08 -0500

    cambio en el control de los errores

--- src/routes/programadores.js
+++ src/routes/programadores.js
@@ -14,11 +14,26 @@ app.post('/programadores', (req, res) => {
     }
 
     const sql = `insert into programadores (nombre,email,password,imagen) values ("${programador.nombre}","${programador.email}","${programador.password}","fdaf")`;
-    mysqlConnection.query(sql, function(err, res) {
-        if (err) throw err;
-        console.log("1 record inserted");
+    mysqlConnection.query(sql, (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Usuario guardado' });
+        } else {
+            console.log("Hubo problema al crear usuario", err);
+        }
     });
 
+});
+
+app.delete('/programadores', function(req, res) {
+    let email = req.body.email;
+    const sql = `delete from programadores where email="${email}"`;
+    mysqlConnection.query(sql, (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Usuario borrado' });
+        } else {
+            console.log("Hubo problema al borrar usuario", err);
+        }
+    });
 });
 
 module.exports = app;
